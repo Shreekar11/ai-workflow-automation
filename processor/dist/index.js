f9ee0151f2880c4653cca0329f7ae607d86f08ae
Shreekar11 <shreekargade2004@gmail.com>
2025-01-06 23:54:52 +0530

    feat: processor implemented to push the task to kafka queue

--- processor/dist/index.js
+++ processor/dist/index.js
@@ -10,10 +10,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const kafkajs_1 = require("kafkajs");
 const client = new client_1.PrismaClient();
+const kafka = new kafkajs_1.Kafka({
+    clientId: "outbox-processor",
+    brokers: ["localhost:9092"],
+});
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        while (1) { }
+        // connects to the kafka broker
+        const producer = kafka.producer();
+        yield producer.connect();
+        //   $ bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+        while (1) {
+            // fetches the pending rows from the outbox table
+            const pendingRows = yield client.zapRunOutBox.findMany({
+                where: {},
+                take: 10,
+            });
+            // the producer sends the zapRunId to the kafka queue
+            producer.send({
+                topic: "zap-events",
+                messages: pendingRows.map((item) => ({ value: item.zapRunId })),
+            });
+            // after the pending rows are sent to the kafka queue, they are deleted from the outbox table
+            yield client.zapRunOutBox.deleteMany({
+                where: {
+                    id: {
+                        in: pendingRows.map((item) => item.id),
+                    },
+                },
+            });
+        }
     });
 }
 main();
--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,9 +1,42 @@
 import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
 
 const client = new PrismaClient();
 
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
 async function main() {
-  while (1) {}
+  // connects to the kafka broker
+  const producer = kafka.producer();
+  await producer.connect();
+
+//   $ bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+
+  while (1) {
+    // fetches the pending rows from the outbox table
+    const pendingRows = await client.zapRunOutBox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    // the producer sends the zapRunId to the kafka queue
+    producer.send({
+      topic: "zap-events",
+      messages: pendingRows.map((item) => ({ value: item.zapRunId })),
+    });
+
+    // after the pending rows are sent to the kafka queue, they are deleted from the outbox table
+    await client.zapRunOutBox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((item) => item.id),
+        },
+      },
+    });
+  }
 }
 
 main();
