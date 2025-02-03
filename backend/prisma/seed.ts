da1cecdf9211838cae358aed1d005cad08753c1c
Shreekar11 <shreekargade2004@gmail.com>
2025-02-03 23:29:01 +0530

    chore: prisma seed updated

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -14,19 +14,19 @@ async function main() {
 
   await prisma.availableAction.create({
     data: {
-      id: "2c50cd8c-0fa6-4265-b0ba-0e895da73606",
-      name: "Email",
+      id: "2656ecd9-a9b1-45ef-9886-9a22a0ae292c",
+      name: "Google Sheets",
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0XvFduY7sDBknSh_lJd80OzsdZ_LaHL2w-g&s",
+        "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/a8/b4bac6e9614670b5a201b62293c489/logo_sheets_2020q4_color_1x_web_512dp.png",
     },
   });
 
   await prisma.availableAction.create({
     data: {
-      id: "2656ecd9-a9b1-45ef-9886-9a22a0ae292c",
-      name: "Google Sheets",
+      id: "2c50cd8c-0fa6-4265-b0ba-0e895da73606",
+      name: "Email",
       image:
-        "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/a8/b4bac6e9614670b5a201b62293c489/logo_sheets_2020q4_color_1x_web_512dp.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0XvFduY7sDBknSh_lJd80OzsdZ_LaHL2w-g&s",
     },
   });
 }
