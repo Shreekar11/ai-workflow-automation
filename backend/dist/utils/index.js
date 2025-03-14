5bc92f828a8cefcb14f45f2cc3048fab562cc69c
Shreekar11 <shreekargade2004@gmail.com>
2025-03-14 15:11:00 +0530

    refactor: webhook secret key notation updated

--- backend/dist/utils/index.js
+++ backend/dist/utils/index.js
@@ -4,7 +4,7 @@ exports.generateRandomString = void 0;
 const generateRandomString = (length = 20) => {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charLength = characters.length;
-    let randomString = "secret_";
+    let randomString = "sk_";
     for (let i = 0; i < length; i++) {
         randomString += characters.charAt(Math.floor(Math.random() * charLength));
     }
--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -2,7 +2,7 @@ export const generateRandomString = (length = 20): string => {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charLength = characters.length;
-  let randomString = "secret_";
+  let randomString = "sk_";
   for (let i = 0; i < length; i++) {
     randomString += characters.charAt(Math.floor(Math.random() * charLength));
   }
